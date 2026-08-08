ba27d74036368a61f90e5a75f571c1a3825a7a38
davekavan888 <davekavan551@gmail.com>
2026-08-08 17:56:11 +0530

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode, useEffect } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+import { useAuthStore } from './stores/auth'
+import './index.css'
+
+const qc = new QueryClient()
+
+function Bootstrap() {
+  const init = useAuthStore((s) => s.init)
+  useEffect(() => { init() }, [init])
+  return <App />
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={qc}>
+      <BrowserRouter>
+        <Bootstrap />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+)
